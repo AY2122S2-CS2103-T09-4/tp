f695aa2d854e381a699ea2e1b042438d802d9172
Lai Mei Tin <e0550504@u.nus.edu>
2022-02-23 20:43:49 +0800

    Create a new RemarkCommand

--- src/main/java/seedu/address/logic/parser/AddressBookParser.java
+++ src/main/java/seedu/address/logic/parser/AddressBookParser.java
@@ -15,6 +15,7 @@ import seedu.address.logic.commands.ExitCommand;
 import seedu.address.logic.commands.FindCommand;
 import seedu.address.logic.commands.HelpCommand;
 import seedu.address.logic.commands.ListCommand;
+import seedu.address.logic.commands.RemarkCommand;
 import seedu.address.logic.parser.exceptions.ParseException;
 
 /**
@@ -55,9 +56,12 @@ public class AddressBookParser {
 
         case ClearCommand.COMMAND_WORD:
             return new ClearCommand();
-
+        
         case FindCommand.COMMAND_WORD:
             return new FindCommandParser().parse(arguments);
+    
+        case RemarkCommand.COMMAND_WORD:
+            return new RemarkCommand();
 
         case ListCommand.COMMAND_WORD:
             return new ListCommand();
