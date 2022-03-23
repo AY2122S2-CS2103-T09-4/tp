7ac13a9bd4914edb371e47fc26f05aac7124cc16
Lai Mei Tin <e0550504@u.nus.edu>
2022-03-23 15:18:18 +0800

    Fix styling issue

--- src/main/java/seedu/ibook/storage/JsonAdaptedProduct.java
+++ src/main/java/seedu/ibook/storage/JsonAdaptedProduct.java
@@ -9,13 +9,11 @@ import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 import seedu.ibook.commons.exceptions.IllegalValueException;
-import seedu.ibook.model.item.UniqueItemList;
 import seedu.ibook.model.product.Category;
 import seedu.ibook.model.product.Description;
 import seedu.ibook.model.product.Name;
 import seedu.ibook.model.product.Price;
 import seedu.ibook.model.product.Product;
-import seedu.ibook.model.product.Product.Item;
 
 /**
  * Jackson-friendly version of {@link Product}.
